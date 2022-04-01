9e4e9fa889d6979cad33efd204d6e514d344c7e3
Tyler Carberry <tyler.carberry@gmail.com>
2022-03-31 20:32:02 -0400

    Add global try/catch to InflationService

--- app/src/main/java/com/tylercarberry/magicmirror/inflation/InflationService.kt
+++ app/src/main/java/com/tylercarberry/magicmirror/inflation/InflationService.kt
@@ -1,5 +1,7 @@
 package com.tylercarberry.magicmirror.inflation
 
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.Jsoup
@@ -13,16 +15,22 @@ class InflationService(
     }
 
     fun getInflationRate(): String? {
-        val request = Request.Builder()
-            .url(URL)
-            .build()
+        try {
+            val request = Request.Builder()
+                .url(URL)
+                .build()
 
-        val response = client.newCall(request).execute()
-        val html = response.body?.string() ?: return null
-        val document = Jsoup.parse(html)
+            val response = client.newCall(request).execute()
+            val html = response.body?.string() ?: return null
+            val document = Jsoup.parse(html)
 
-        val price = document.getElementsByClass("cpi")[4].text()
-        return price.split(" ")[1].replace("+", "").trim()
+            val price = document.getElementsByClass("cpi")[4].text()
+            return price.split(" ")[1].replace("+", "").trim()
+        } catch (e: Exception) {
+            Log.e("InflationService", "Error fetching inflation rate", e)
+            FirebaseCrashlytics.getInstance().recordException(e)
+            return null
+        }
     }
 
 }
