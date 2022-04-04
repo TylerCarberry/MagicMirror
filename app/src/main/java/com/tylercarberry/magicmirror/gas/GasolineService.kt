0968b7955d4c8aeab840c7ec369fb95500e35afc
Tyler Carberry <tyler.carberry@gmail.com>
2022-04-04 13:01:43 -0400

    Catch IOException for gasoline prices

--- app/src/main/java/com/tylercarberry/magicmirror/gas/GasolineService.kt
+++ app/src/main/java/com/tylercarberry/magicmirror/gas/GasolineService.kt
@@ -1,28 +1,38 @@
 package com.tylercarberry.magicmirror.gas
 
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.Jsoup
+import java.io.IOException
 
 class GasolineService(
     private val client: OkHttpClient
 ) {
 
     companion object {
+        private const val TAG = "GasolineService"
         private const val URL = "https://gasprices.aaa.com/"
     }
 
     fun getNationalGasPrice(): Double? {
-        val request = Request.Builder()
-            .url(URL)
-            .build()
+        try {
+            val request = Request.Builder()
+                .url(URL)
+                .build()
 
-        val response = client.newCall(request).execute()
-        val html = response.body?.string() ?: return null
-        val document = Jsoup.parse(html)
+            val response = client.newCall(request).execute()
+            val html = response.body?.string() ?: return null
+            val document = Jsoup.parse(html)
 
-        val price = document.getElementsByClass("numb")[0].text().replace("$", "")
-        return price.toDoubleOrNull()
+            val price = document.getElementsByClass("numb")[0].text().replace("$", "")
+            return price.toDoubleOrNull()
+        } catch (e: IOException) {
+            Log.e(TAG, "Error fetching gas price", e)
+            FirebaseCrashlytics.getInstance().recordException(e)
+            return null
+        }
     }
 
 }
